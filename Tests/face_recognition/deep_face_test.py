fcf1146083b3a0edd2cfac52e8c6e7498df85fb9
Giacomo Detomaso <giacomodetomaso24@gmail.com>
2023-03-23 10:32:23 +0100

    Added a Deepface overview

--- Tests/face_recognition/deep_face_test.py
+++ Tests/face_recognition/deep_face_test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy
 import os
 import pandas
+import cv2
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
@@ -16,9 +17,9 @@ detectors_backends = [
 ]
 
 # face detection and alignment
-face_objs = df.extract_faces(img_path="assets/biden.jpg", target_size=(224, 224), detector_backend=detectors_backends[4])
+face_objs = df.extract_faces(img_path="assets/trump.jpg", target_size=(224, 224), detector_backend=detectors_backends[5], enforce_detection=False)
 print(face_objs)
 plt.imshow(face_objs[0]['face'])
 plt.show()
 
-print(pandas.DataFrame(df.analyze(img_path="assets/biden.jpg", detector_backend=detectors_backends[4])).to_string())
+#print(pandas.DataFrame(df.analyze(img_path="assets/biden.jpg", detector_backend=detectors_backends[4])).to_string())
